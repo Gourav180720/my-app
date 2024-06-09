5509699536c48bc6591e03d75bfc5e8462ac935d
Gourav1807 <gourav180720@gmail.com>
2024-06-09 12:18:10 +0530

    Context Api  Project

---
+++ src/module1.mjs
@@ -0,0 +1,10 @@
+
+const a = "Gourav The Great";
+const b = "Shivangi";
+const c = "Ayush ";
+const d = " Sanskar ";
+
+export default b;
+export {a};
+export {c};
+export {d};
---
+++ src/module2.mjs
@@ -0,0 +1,10 @@
+
+import dza, {a,c,d} from './module2.mjs'
+console.log(dza);
+
+console.log(c);
+console.log(d);
+
+console.log(z);
+
+
